0a7b1c3bc4e541072735ec3cec435c72fe5f2b69
Juliana Marin <carmenmarin689@gmail.com>
2025-06-17 14:22:16 -0500

    update pages

---
+++ pages/pagina6.py
@@ -0,0 +1,16 @@
+from estilos import crear_header_corporativo
+from login import set_background
+from estilos import aplicar_estilos
+import streamlit as st
+import login
+ 
+st.set_page_config(layout="wide")
+login.generarLogin()
+set_background("images/fondo4.jpg")
+if 'usuario' in st.session_state:
+    crear_header_corporativo(
+        "🛍️ KPIs ÁREA ADMINISTRATIVA",
+        "Indicadores para el área"
+    )
+
+    aplicar_estilos()
---
+++ pages/pagina9.py
@@ -0,0 +1,16 @@
+from estilos import crear_header_corporativo
+from login import set_background
+from estilos import aplicar_estilos
+import streamlit as st
+import login
+ 
+st.set_page_config(layout="wide")
+login.generarLogin()
+set_background("images/fondo4.jpg")
+if 'usuario' in st.session_state:
+    crear_header_corporativo(
+        "KPIs ÁREA ARQUITECTURA",
+        "Indicadores para el área"
+    )
+
+    aplicar_estilos()
